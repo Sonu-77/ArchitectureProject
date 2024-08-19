3d3bea37f53abe2a0ae172f15dace3c4f3f9daf8
Sonu Kachhap <166591588+Sonu-77@users.noreply.github.com>
2024-08-19 08:34:58 +0000

    Project creating

---
+++ Aritecture_Project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Herobanner from './components/Herobanner'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Navbar></Navbar>
+      <Herobanner/>
+    </>
+  )
+}
+
+export default App
---
+++ Aritecture_Project/src/components/Herobanner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='w-full h-[40vw] bg-[url("https://www.rakenapp.com/img/asset/YXNzZXRzL2NsaW1hdGUtY2hhbmdlLWhlcm8uanBn?w=1280&h=720&fit=crop&fm=webp&q=85&s=96364cf523fde3e0726e053a070fe322")] flex flex-col items-center justify-center bg-cover bg-center mt-2 opacity-[1] relative'>
+        <div className='absolute left-[24vw]'>
+
+            <div>
+                <h2 className='text-[2.3vw] font-bold leading-[3vw]'>ARCHITECT <br />BUILDER CONSTRUCTION <br />SERVICES</h2>
+            </div>
+            <div className='flex  gap-[1vw] mt-[1.4vw] '>
+                <button className='text-[1vw] w-[10vw] text-[#ffff] bg-[#f07b27] p-[0.4vw] rounded '>Contact Us</button>
+                <button className='text-[1vw] text-[#ffff] w-[10vw] bg-[#000000] p-[0.4vw] rounded '>About</button>
+            </div>
+        </div>
+    </div>
+  )
+}
